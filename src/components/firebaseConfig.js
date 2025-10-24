1ed814cba190f209e8f3b82cdde561ac81827b3c
Yuthish31 <yuthish2222k0156@gmail.com>
2025-10-25 04:11:23 +0530

    Initial commit: QR Code Generator with Firebase setup

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBInzBlOiZvACXVAOpIjs6eG1ruSi0Mkls",
-  authDomain: "qr-code-generator-a5512.firebaseapp.com",
-  projectId: "qr-code-generator-a5512",
-  storageBucket: "qr-code-generator-a5512.appspot.com",
-  messagingSenderId: "1031616590522",
-  appId: "1:1031616590522:web:d184526f8e8ef08c9d4883",
-  measurementId: "G-ZHP35RBMY6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
